f9f0fce371c5163deedba2a558e822b745e1eab1
Rob Franceschini <robert.franceschini@gmail.com>
2016-01-16 21:22:27 -0500

    Initial commit to own repo

---
+++ js/project.js
@@ -0,0 +1,73 @@
+var playerHealth = 20;
+var blobHealth = 20;
+
+$('#p-health').text(playerHealth);
+$('#attack-btn').on("click", fightBlob);
+
+function fightBlob () {
+	if (playerHealth <= 0) {
+		$('#action').text('You cannot attack because you are dead!');
+	}
+	else if (blobHealth > 0) {
+		$('#attack-btn').off();
+		$('#attack-btn').text('Wait...');
+		var youHit = Math.floor(Math.random() * 10);
+		blobHealth = blobHealth - youHit;
+		$('#action').text('You attack the green blob for ' + youHit + ' damage! ' + blobHealth + ' health left!');	
+		$('#slice').css('height', '40%');
+		$('figure').css('background-color', 'red');
+		setTimeout(function() {
+		$('#slice').css('display', 'none');	
+		$('#slice').css('height', '0');
+		$('figure').css('background-color', 'darkolivegreen');
+		},700);
+		setTimeout(function() {
+		$('#slice').css('display', 'initial');
+		},800);
+		if (blobHealth <= 0) {
+			setTimeout(function() {
+			$('#action').text('The green blob is defeated!');
+			$('.blob').fadeOut();
+			$('#attack-btn').on("click", fightBlob);
+			$('#attack-btn').text('Attack');
+			$('#center-alert')
+			.text('You win!')
+			.fadeIn();
+			},2000);
+		}
+		else {
+			var blobHit = Math.floor(Math.random() * 10);
+			playerHealth = playerHealth - blobHit;
+			setTimeout(function() {
+			$('#action').text('The green blob attacks! ' + blobHit + ' damage!');
+			$('.click-anim').css('transform', 'scale(1.2)');
+			$('#burst-8').fadeIn();
+			setTimeout(function() {
+			$('.click-anim').css('transform', 'scale(1.0)');
+			$('#burst-8').fadeOut();
+			},700);
+			$('#p-health').text(playerHealth);
+			},2000);
+			if (playerHealth > 0) {
+				setTimeout(function() {
+				 $('#action').text('Your Turn!');
+				 $('#attack-btn').on("click", fightBlob);
+				 $('#attack-btn').text('Attack');
+				},4000);
+			}
+			else {
+				setTimeout(function() {
+				 $('#action').text('The green blob defeats you!');
+				 $('#attack-btn').on("click", fightBlob);
+				 $('#attack-btn').text('Attack');
+				 $('#center-alert')
+				.text('You are dead!')
+				.fadeIn();
+				},4000);
+			}
+		}
+	}
+	else {
+		$('#action').text('You swing your sword and slice the empty air!');
+	}
+}
